3f8536e764c93b7d028cc13e74e2f321a1106d06
joshcarp <joshuacarpeggiani@gmail.com>
2021-11-22 21:18:04 +1100

    Remove complex old cli

--- generate.go
+++
@@ -1,5 +0,0 @@
-//go:generate sh -c "cat generic.go | genny gen \"_Config=Config _Something=Service\" > config_service.go"
-//go:generate sh -c "cat generic.go | genny gen \"_Config=Config _Something=Context\" > config_context.go"
-//go:generate sh -c "cat generic.go | genny gen \"_Config=Config _Something=User\" > config_user.go"
-//go:generate sh -c "cat generic.go | genny gen \"_Config=Service _Something=Environment\" > config_service_environment.go"
-package grpctl
